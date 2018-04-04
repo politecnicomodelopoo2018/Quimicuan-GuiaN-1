e21a8101818763f042d66b57a60c4b3559a3f1de
yisusyrama <quinterosyisus@gmail.com>
2018-04-04 17:40:11 -0300

    Ej 4 s

---
+++ TestExampleONE.py
@@ -0,0 +1,61 @@
+from TestExampleONEPersona import Persona
+from TestExampleONERegistro import Registro
+
+Registro1 = Registro()
+
+PesoR1 = input()
+AlturaR1 = input()
+AñoR1 = input()
+MesR1 = input()
+DiaR1 = input()
+
+Registro1.SetPeso(PesoR1)
+Registro1.SetAltura(AlturaR1)
+Registro1.SetAñoReg(AñoR1)
+Registro1.SetMesReg(MesR1)
+Registro1.SetDiaReg(DiaR1)
+
+Registro2 = Registro()
+
+PesoR2 = input()
+AlturaR2 = input()
+AñoR2 = input()
+MesR2 = input()
+DiaR2 = input()
+
+Registro1.SetPeso(PesoR2)
+Registro1.SetAltura(AlturaR2)
+Registro1.SetAñoReg(AñoR2)
+Registro1.SetMesReg(MesR2)
+Registro1.SetDiaReg(DiaR2)
+
+Persona1 = Persona()
+
+Nombre1 = input()
+Apellido1 = input()
+FechaNac1 = input()
+
+Persona1.SetNombre(Nombre1)
+Persona1.SetApellido(Apellido1)
+Persona1.SetFechaNac(FechaNac1)
+
+Persona1.AgregarRegistro(Registro1)
+Persona1.AgregarRegistro(Registro2)
+
+AñoR = input()
+MesR = input()
+DiaR = input()
+
+Persona1.GetRegistro(DiaR, MesR, AñoR)
+
+Año = input()
+
+Persona1.PromedioReg(Año)
+
+AñoPrim = input()
+AñoSegun = input()
+
+Persona1.AlturaAños(AñoPrim, AñoSegun)
+
+
+
