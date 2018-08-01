5f2d22ebd4bb57042f8d72c6525afbc3a20d019f
yisusyrama <quinterosyisus@gmail.com>
2018-08-01 17:43:16 -0300

    PrimerTrabajoPractico

--- Main_SQL.py
+++ Main_SQL.py
@@ -5,8 +5,45 @@ DB().SetConnection('127.0.0.1','root', 'alumno', 'quinteros')
 
 Poder = Poder()
 
-Poder.SetId(1)
-Poder.SetDescripcion("Rayo")
-Poder.Insert()
+print("Poder -> Alta -> 0")
+print("Poder -> Baja -> 1")
+print("Poder -> Modificacion ->2")
 
-runDB = DB().run("SELECT * FROM Poder")
+Menu = None
+
+while Menu != 3 :
+
+    Menu = int(input())
+
+    if Menu == 0:
+
+        print("Descripcion Del Poder")
+
+        Descripcion = input()
+
+        Poder.SetDescripcion(Descripcion)
+        Poder.Insert()
+
+        runDB = DB().run("SELECT * FROM Poder")
+
+    if Menu == 2:
+
+
+
+        print("ID Del Poder A Modificar")
+
+        idUpdate = int(input())
+
+        print("Nueva Descripcion")
+
+        DescRemp = input()
+
+        Poder.Update(DescRemp, idUpdate)
+
+    if Menu == 1:
+
+        print("ID Del Poder A Eliminar")
+
+        idDelete = int(input())
+
+        Poder.Delete(idDelete)
--- Py_SQL_Poder.py
+++ Py_SQL_Poder.py
@@ -4,11 +4,14 @@ class Poder(object):
     id = None
     descripcion = None
 
-    def SetId(self, id):
-        self.id = id
-
     def SetDescripcion(self, Descripcion):
         self.descripcion = Descripcion
 
     def Insert(self):
-        DB().run("INSERT INTO Poder VALUES (%d,'%s')" % (self.id, self.descripcion))
+        DB().run("INSERT INTO Poder (Descripcion_Poder) VALUES ('%s')" % (self.descripcion))
+
+    def Update(self, DescRemp, idUpdate):
+        DB().run("UPDATE Poder SET Descripcion_Poder = ('%s') WHERE idPoder = ('%d')" % (DescRemp, idUpdate))
+
+    def Delete(self, idDelete):
+        DB().run("DELETE FROM Poder WHERE idPoder = ('%d')" % (idDelete))
